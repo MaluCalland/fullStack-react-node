83ddceec4dcd313b8f80e3bdf332d9b2ef636d34
MaluCalland <malucalland@gmail.com>
2025-07-07 17:22:01 -0300

    adicionando barra de pesquisa

--- react-livros/src/App.js
+++ react-livros/src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import Pesquisa from './components/Pesquisa';
 import styled from 'styled-components'; //ajuda a criar containers (encapsulam conteudos, guardado seus elementos)
 
 const AppConteiner = styled.div`
@@ -11,6 +12,7 @@ function App() {
   return (
     <AppConteiner>
       <Header/>
+      <Pesquisa/>
     </AppConteiner>
   );
 }
