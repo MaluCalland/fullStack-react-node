17f0b173bf5c397f4723d14155b193d00881ccca
MaluCalland <malucalland@gmail.com>
2025-07-16 16:13:40 -0300

    ajustando cores

--- react-livros/src/routes/Favoritos.js
+++ react-livros/src/routes/Favoritos.js
@@ -1,11 +1,10 @@
 import Pesquisa from '../components/Pesquisa';
 import styled from 'styled-components'; //ajuda a criar containers (encapsulam conteudos, guardado seus elementos)
-import UltimosLancamentos from '../components/UltimosLancamentos';
 
 const AppConteiner = styled.div`
     width: 100vw;
     height: 100vh;
-    background-image: linear-gradient(90deg, #bee3ff 35%, #326589);
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 `
 
 function Favoritos() {
--- react-livros/src/routes/Home.js
+++ react-livros/src/routes/Home.js
@@ -5,7 +5,7 @@ import UltimosLancamentos from '../components/UltimosLancamentos';
 const AppConteiner = styled.div`
     width: 100vw;
     height: 100vh;
-    background-image: linear-gradient(90deg, #bee3ff 35%, #326589);
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 `
 
 function Home() {
