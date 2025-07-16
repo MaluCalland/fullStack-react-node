47286b397fa5c27bef8c22f8da3f61b71c67bc3b
MaluCalland <malucalland@gmail.com>
2025-07-16 16:08:02 -0300

    criando servico de livros com axios para conectar com os dados

---
+++ react-livros/src/services/livros.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+//criando api para fazer as chamadas get, post, delete...
+
+//variavel de conexao com a API
+const livrosAPI = axios.create({baseURL: "http://localhost:8000/livros"}) //api de livros
+//axios.create() - comeca a criar uma API com o axios
+//baeURL - deve ser igual a URL onde o Axios vai buscar os dados
+
+//pegar dados - coletar e ler os dados da API
+function getLivros() {
+    const response = livrosAPI.get('/') //retornar os livros
+
+    return response.data
+}
+
+export {
+    getLivros
+}
