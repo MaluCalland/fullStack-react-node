a8b68795a1c79c16f0e991251cdc292d847df9b4
MaluCalland <malucalland@gmail.com>
2025-07-17 17:54:42 -0300

    rotas de livros favoritos

--- back-server-livros/app.js
+++ back-server-livros/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const rotaLivro = require("./routes/livro.js");
+const rotaFavorito = require("./routes/favoritos.js");
+
 const cors = require('cors');
 
 const app = express(); //minha aplicacao Express - cria uma aplicacao express usando express()
-
 app.use(express.json()) //habilita o uso do body JSON 
 app.use(cors({ origin: "*" }))
 
-app.use("/livros", rotaLivro) 
+app.use("/livros", rotaLivro)
+app.use("/favoritos", rotaFavorito) 
 
 const port = 8000 //front com porta 3000
 
