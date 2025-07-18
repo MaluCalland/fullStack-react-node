17b475b6f3719c99f84a6b7df0130bacb05102d4
MaluCalland <malucalland@gmail.com>
2025-07-18 00:13:39 -0300

    estilizando pagina de Favoritos

--- react-livros/src/routes/Favoritos.js
+++ react-livros/src/routes/Favoritos.js
@@ -1,11 +1,46 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components'; //ajuda a criar containers (encapsulam conteudos, guardado seus elementos)
 import { getFavoritos } from '../services/favoritos';
+import livroImg from '../image/livro.png'
 
-const AppConteiner = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+const AppContainer = styled.div`
+   width: 100vw;
+   height: 100vh;
+   background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
+`
+
+const ResultadoContainer = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: center;
+`
+
+const Resultado = styled.div`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin: 20px 0;
+   cursor: pointer;
+   text-align: center;
+   padding: 0 100px;
+   p {
+       width: 200px;
+       color: #FFF;
+   }
+   img {
+       width: 100px;
+   }
+   &:hover {
+       border: 1px solid white;
+   }
+`
+
+const Titulo = styled.h2`
+   color: #FFF;
+   font-size: 36px;
+   text-align: center;
+   width: 100%;
+   padding-top: 35px
 `
 
 function Favoritos() {
@@ -22,11 +57,21 @@ function Favoritos() {
   }, [])
 
   return (
-    <AppConteiner>
-      {favoritos.map( favorito => (
-        <p>{favorito.nome}</p>
-      ))}
-    </AppConteiner>
+   <AppContainer>
+     <div>
+       <Titulo>Aqui estão seus livros favoritos:</Titulo>
+       <ResultadoContainer>
+         {
+           favoritos.length !== 0 ? favoritos.map(favorito => (
+             <Resultado>
+               <img src={livroImg}/>
+               <p>{favorito.nome}</p>
+             </Resultado>
+           )) : null
+         }
+       </ResultadoContainer>
+     </div>
+   </AppContainer>
   );
 }
 
